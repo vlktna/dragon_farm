e24aa491884383534b5c929267626d33ac3acd06
vlktna <v.volokitinaa@gmail.com>
2022-01-24 01:25:34 +0200

    Wrong password response

--- src/main/kotlin/com/dragon/farm/coursework/endpoint/exception/RestResponseEntityExceptionHandler.kt
+++ src/main/kotlin/com/dragon/farm/coursework/endpoint/exception/RestResponseEntityExceptionHandler.kt
@@ -2,12 +2,12 @@ package com.dragon.farm.coursework.endpoint.exception
 
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.security.authentication.BadCredentialsException
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.context.request.WebRequest
 import org.springframework.web.server.ResponseStatusException
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
-import javax.security.sasl.AuthenticationException
 
 @ControllerAdvice
 class RestResponseEntityExceptionHandler: ResponseEntityExceptionHandler() {
@@ -17,8 +17,8 @@ class RestResponseEntityExceptionHandler: ResponseEntityExceptionHandler() {
         return ResponseEntity<ExceptionResponse>(ExceptionResponse(ex.reason, ex.status), ex.status)
     }
 
-    @ExceptionHandler(value = [AuthenticationException::class, AuthenticationException::class])
-    protected fun handleAuthenticationException(ex: AuthenticationException, request: WebRequest): ResponseEntity<ExceptionResponse> {
-        return ResponseEntity<ExceptionResponse>(ExceptionResponse(ex.message, HttpStatus.UNAUTHORIZED), HttpStatus.UNAUTHORIZED)
+    @ExceptionHandler(value = [BadCredentialsException::class, BadCredentialsException::class])
+    protected fun handleAuthenticationException(ex: BadCredentialsException, request: WebRequest): ResponseEntity<ExceptionResponse> {
+        return ResponseEntity<ExceptionResponse>(ExceptionResponse("Wrong password", HttpStatus.UNAUTHORIZED), HttpStatus.UNAUTHORIZED)
     }
 }
